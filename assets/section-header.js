02e869e824787e781ef488a763d496e16f62bbce
Dan Hemerlein <danhemerlein@gmail.com>
2025-08-11 22:46:12 -0400

    fix sticky header issues

--- assets/section-header.js
+++ assets/section-header.js
@@ -17,106 +17,145 @@ if (typeof MainHeader !== "function") {
 
       // Sticky header
 
-      if (this.hasAttribute("data-sticky-header")) {
-        const stickyHeader = document.createElement("div");
-        stickyHeader.classList = "sticky-header site-header";
+      // if (this.hasAttribute("data-sticky-header")) {
+      //   const stickyHeader = document.createElement("div");
+      //   stickyHeader.classList = "sticky-header site-header";
 
-        if (this.classList.contains("site-header--style-one-row")) {
-          stickyHeader.innerHTML = `<div class="container--large">
-      			<div class="${
-              this.querySelector(".header__top").className
-            }" style="height:calc(var(--header-logo) + var(--header-vertical-space))">
-      				${this.querySelector(".header__top").innerHTML}
-      			</div>
-      		</div>`;
-          stickyHeader.className = `sticky-header ${this.className}`;
-          stickyHeader.classList.remove("site-header--absolute");
-        } else {
-          stickyHeader.innerHTML = `<div class="header__bottom header-container container--large portable-hide">
-      			${this.querySelector(".header__bottom").innerHTML}
-      			<div style="display:flex;gap:15px;">
-      				${
-                this.querySelector(".area--custom-icon")
-                  ? this.querySelector(".area--custom-icon").outerHTML
-                  : ""
+      //   if (this.classList.contains("site-header--style-one-row")) {
+      //     stickyHeader.innerHTML = `<div class="container--large">
+      // 			<div class="${
+      //         this.querySelector(".header__top").className
+      //       }" style="height:calc(var(--header-logo) + var(--header-vertical-space))">
+      // 				${this.querySelector(".header__top").innerHTML}
+      // 			</div>
+      // 		</div>`;
+      //     stickyHeader.className = `sticky-header ${this.className}`;
+      //     stickyHeader.classList.remove("site-header--absolute");
+      //   } else {
+      //     stickyHeader.innerHTML = `<div class="header__bottom header-container container--large portable-hide">
+      // 			${this.querySelector(".header__bottom").innerHTML}
+      // 			<div style="display:flex;gap:15px;">
+      // 				${
+      //           this.querySelector(".area--custom-icon")
+      //             ? this.querySelector(".area--custom-icon").outerHTML
+      //             : ""
+      //         }
+      // 				${this.querySelector(".area--search").outerHTML}
+      // 				${
+      //           this.querySelector(".area--account")
+      //             ? this.querySelector(".area--account").outerHTML
+      //             : ""
+      //         }
+      // 				${this.querySelector(".area--cart").outerHTML}
+      // 			</div>
+      // 		</div>
+      // 		<div class="site-header element--hide-on-portable-plus">
+      // 			${this.querySelector(".header-container--top").innerHTML}
+      // 		</div>`;
+      //   }
+
+      //   document.body.append(stickyHeader);
+
+      //   stickyHeader.querySelectorAll("[id]").forEach((elm) => {
+      //     elm.id = `${elm.id}-sticky`;
+      //   });
+      //   stickyHeader
+      //     .querySelectorAll(".site-nav [aria-controls]")
+      //     .forEach((elm) => {
+      //       elm.setAttribute(
+      //         "aria-controls",
+      //         `${elm.getAttribute("aria-controls")}-sticky`
+      //       );
+      //     });
+
+      //   window.lst = window.scrollY;
+      //   window.lhp = 0;
+
+      //   const stickyHeaderDeskBound = this.classList.contains(
+      //     "site-header--style-one-row"
+      //   )
+      //     ? this.querySelector(".header__top")
+      //     : this.querySelector(".header__bottom");
+      //   const stickyHeaderMobileBound = this.querySelector(".header__top");
+
+      //   this.SCROLL_StickyHelper = () => {
+      //     var st = window.scrollY;
+
+      //     if (
+      //       (st <= 0 ||
+      //         (window.innerWidth >= 1024
+      //           ? stickyHeaderDeskBound.getBoundingClientRect().top >= 0
+      //           : stickyHeaderMobileBound.getBoundingClientRect().top >= 0)) &&
+      //       stickyHeader.classList.contains("show")
+      //     ) {
+      //       stickyHeader.classList.remove("show");
+      //       return;
+      //     }
+
+      //     if (st < 0 || Math.abs(lst - st) <= 5) return;
+
+      //     if (st > window.lhp) {
+      //       if (st == 0 && stickyHeader.classList.contains("show")) {
+      //         stickyHeader.classList.remove("show");
+      //       } else if (st <= lst && !stickyHeader.classList.contains("show")) {
+      //         window.lhp = stickyHeader.offsetTop;
+      //         if (
+      //           document.body.id !=
+      //           "create-your-bundle-subscribe-amp-save-up-to-25-off"
+      //         ) {
+      //           stickyHeader.classList.add("show");
+      //         }
+      //       } else if (st > lst && stickyHeader.classList.contains("show")) {
+      //         stickyHeader.classList.remove("show");
+      //       }
+      //     }
+
+      //     window.lst = st;
+      //   };
+
+      //   window.addEventListener("scroll", this.SCROLL_StickyHelper, {
+      //     passive: true,
+      //   });
+      // }
+
+      // sticky header
+
+      const header = document.querySelector(".site-header");
+      const announcementBar = document.querySelector("announcement-bar");
+
+      // const headerHeight = header.offsetHeight;
+
+      let lastScrollY = window.scrollY;
+      let ticking = false;
+
+      window.addEventListener("scroll", () => {
+        if (!ticking) {
+          requestAnimationFrame(() => {
+            const currentScrollY = window.scrollY;
+
+            // If at the top of the document, always show header
+            if (currentScrollY < 10) {
+              header.classList.add("show");
+              announcementBar.classList.add("show");
+            } else {
+              // Check scroll direction
+              if (currentScrollY < lastScrollY) {
+                // Scrolling up - show header
+                header.classList.add("show");
+                announcementBar.classList.add("show");
+              } else if (currentScrollY > lastScrollY) {
+                // Scrolling down - hide header
+                header.classList.remove("show");
+                announcementBar.classList.remove("show");
               }
-      				${this.querySelector(".area--search").outerHTML}
-      				${
-                this.querySelector(".area--account")
-                  ? this.querySelector(".area--account").outerHTML
-                  : ""
-              }
-      				${this.querySelector(".area--cart").outerHTML}
-      			</div>
-      		</div>
-      		<div class="site-header element--hide-on-portable-plus">
-      			${this.querySelector(".header-container--top").innerHTML}
-      		</div>`;
-        }
-
-        document.body.append(stickyHeader);
-
-        stickyHeader.querySelectorAll("[id]").forEach((elm) => {
-          elm.id = `${elm.id}-sticky`;
-        });
-        stickyHeader
-          .querySelectorAll(".site-nav [aria-controls]")
-          .forEach((elm) => {
-            elm.setAttribute(
-              "aria-controls",
-              `${elm.getAttribute("aria-controls")}-sticky`
-            );
-          });
-
-        window.lst = window.scrollY;
-        window.lhp = 0;
-
-        const stickyHeaderDeskBound = this.classList.contains(
-          "site-header--style-one-row"
-        )
-          ? this.querySelector(".header__top")
-          : this.querySelector(".header__bottom");
-        const stickyHeaderMobileBound = this.querySelector(".header__top");
-
-        this.SCROLL_StickyHelper = () => {
-          var st = window.scrollY;
-
-          if (
-            (st <= 0 ||
-              (window.innerWidth >= 1024
-                ? stickyHeaderDeskBound.getBoundingClientRect().top >= 0
-                : stickyHeaderMobileBound.getBoundingClientRect().top >= 0)) &&
-            stickyHeader.classList.contains("show")
-          ) {
-            stickyHeader.classList.remove("show");
-            return;
-          }
-
-          if (st < 0 || Math.abs(lst - st) <= 5) return;
-
-          if (st > window.lhp) {
-            if (st == 0 && stickyHeader.classList.contains("show")) {
-              stickyHeader.classList.remove("show");
-            } else if (st <= lst && !stickyHeader.classList.contains("show")) {
-              window.lhp = stickyHeader.offsetTop;
-              if (
-                document.body.id !=
-                "create-your-bundle-subscribe-amp-save-up-to-25-off"
-              ) {
-                stickyHeader.classList.add("show");
-              }
-            } else if (st > lst && stickyHeader.classList.contains("show")) {
-              stickyHeader.classList.remove("show");
             }
-          }
 
-          window.lst = st;
-        };
-
-        window.addEventListener("scroll", this.SCROLL_StickyHelper, {
-          passive: true,
-        });
-      }
+            lastScrollY = currentScrollY;
+            ticking = false;
+          });
+          ticking = true;
+        }
+      });
 
       // drawer connections
 
